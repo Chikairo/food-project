5c2ed0eabe30fe49ac279c1ff265a650b33d605e
Kairo <blessingchikairo@gmail.com>
2025-02-27 21:30:55 +0100

    Update checkout.js

--- script/checkout.js
+++ script/checkout.js
@@ -2,7 +2,7 @@ import {renderOrderSummary} from "./checkout/order.js";
 import {renderPaymentSumary} from "./checkout/payment.js";
 import { loadProducts } from "../data/products.js";
 
-loadProducts(fun, () => {
+loadProducts( () => {
     renderOrderSummary();
 
     renderPaymentSumary();
